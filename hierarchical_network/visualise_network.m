0710a0b3434cfd863465754783037333c9c1f691
alex.hs.lee <alex.hs.lee@gmail.com>
2014-07-17 11:22:21 +1000

    Visually looking OK-ish, some problems with top of figure being cut off but otherwise OK, need to do more important code now, like actually getting the neuron to learn stuff...

--- hierarchical_network/visualise_network.m
+++ hierarchical_network/visualise_network.m
@@ -18,56 +18,67 @@
 %     Some code may have been taken from Izhikevich (2003) Which neuron model
 %     Some code may have been taken from Robert Quinn's DVS work, in particular
 %     displayingdata.m
-
-% Load DVS events into matrix otf [x, y, pol, t]
-events = getEvents()
-
-% The amount of time between each frame? Hard-coded to 10000 microseconds
-% in Rob's original code.
-global FRAMETIME = 10000;
-
-%get dimensions of events matrix
-[xsize, ysize] = size(events);
-%set figure properties
-video = figure('color','white');
-%find out the starting timestamp (in microseconds)
-plottime = events(1,4)
-
-%set entire events matrix to int32
-% Not sure why this is necessary.
-events = int32(events);
-
-%set figure to grayscale
-colormap(gray)
-
-for j = 1:xsize
-    % Break data into FRAMETIME microsecond blocks
-    ind = find(events(:,4) >= plottime & events(:,4)<=plottime+FRAMETIME);
-    plottime = plottime + FRAMETIME;
-    
-    %set up the background for the matrix to display
-    background = zeros(128,128);
-   
-   %for each set of FRAMETIME microsecond blocks of data, adjust the zeros
-   %matrix to account for the changed events in that time block (-1 or 1)
-   %and finally adjust to values between 0 and 255 (grayscale values)
-   for k = ind(1,1):ind(size(ind),1)
-       background(events(k,2)+1, events(k,1)+1) = events(k,3);
-   end
-   
-   %set up the escape root for the for loop (I need to change this)
-    if plottime > events(xsize,4)
-        break;
+function visualise_network(events)
+    if nargin == 0, % no events matrix provided
+        % Load DVS events into matrix otf [x, y, pol, t]
+        events = getEvents();
     end
-    %end
-    %set the size of the image
-    xlim([0, 128])
-    ylim([0, 128])
     
-    %display the stuff in real time (not sure if necessary yet)
-    drawnow;
-    %flip the images the correct way around again
-    background = flipud(background);
-    %actually display the images at the speed of the for loop
-    imagesc(background);
+    % The amount of time between each frame? Hard-coded to 10000 microseconds
+    % in Rob's original code.
+    FRAMETIME = 10000;
+    
+    % --- Set up for DVS video -----------------------------------------------
+    %get dimensions of events matrix
+    [xsize, ysize] = size(events);
+    %set figure properties
+    video = figure('color','white');
+    set(video, 'Name', 'Visualise Network', 'Units','normalized', ...
+        'Position', [0.05, 0.45, 0.5, 0.5]); 
+    %find out the starting timestamp (in microseconds)
+    plottime = events(1,4)
+
+    %set entire events matrix to int32
+    % Not sure why this is necessary.
+    events = int32(events);
+
+    %set figure to grayscale
+    colormap(gray)
+    background = subplot(4, 2, 1);
+    set(background, 'Units', 'normalized', 'Position', [0.05 0.45 0.5 0.5]);
+    axis equal
+    %---- Set up for neuron firing plot --------------------------------------
+
+
+    for j = 1:xsize
+        % Break data into FRAMETIME microsecond blocks
+        ind = find(events(:,4) >= plottime & events(:,4)<=plottime+FRAMETIME);
+        plottime = plottime + FRAMETIME;
+
+        %set up the background for the matrix to display
+        background = zeros(128,128);
+
+       %for each set of FRAMETIME microsecond blocks of data, adjust the zeros
+       %matrix to account for the changed events in that time block (-1 or 1)
+       %and finally adjust to values between 0 and 255 (grayscale values)
+       for k = ind(1,1):ind(size(ind),1)
+           background(events(k,2)+1, events(k,1)+1) = events(k,3);
+       end
+
+       %set up the escape root for the for loop (I need to change this)
+        if plottime > events(xsize,4)
+            break;
+        end
+        %end
+        %set the size of the image
+        xlim([0, 128])
+        ylim([0, 128])
+
+        %display the stuff in real time (not sure if necessary yet)
+        drawnow;
+        %flip the images the correct way around again
+        background = flipud(background);
+        %actually display the images at the speed of the for loop
+        imagesc(background);
+    end
 end
